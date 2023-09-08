94d69a8418a6c191b62d8703a6e41529afad92f2
benmiano <mianoben97@gmail.com>
2023-09-08 17:13:07 +0300

    static files update

--- sso/settings.py
+++ sso/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL =  'sso/static/'
-STATICFILES_DIRS=[BASE_DIR / 'sso/static']
+STATICFILES_DIRS=[BASE_DIR / 'sso/static/']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
